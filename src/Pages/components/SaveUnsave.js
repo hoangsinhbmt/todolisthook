f5c7cea52e93baad16ead523cde13e2993b17d15
Hoàng Sinh <74650229+hoangsinhbmt@users.noreply.github.com>
2022-05-10 13:59:07 +0700

    fix savetolocal

--- src/Pages/components/SaveUnsave.js
+++ src/Pages/components/SaveUnsave.js
@@ -1,20 +1,21 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { UseStore, actions } from "../store";
 
 export default function SaveUnsave() {
   const [state, dispatch] = UseStore();
-  const { job, jobs, doneJob } = state;
+  // const { job, jobs, doneJob } = state;
   const SaveList = () => {
     dispatch(actions.saveTasks());
     alert("bạn đã lưu thành công");
   };
   const ResetList = () => {
-    useEffect(dispatch(actions.unsaveTask()))
+    dispatch(actions.unsaveTask())
+    dispatch(actions.setJob(""));
   };
   return (
     <>
       <button
-        className=" absolute bottom-20 right-10 bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded"
+        className=" absolute bottom-24 right-10 bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded"
         onClick={SaveList}
       >
         Save
